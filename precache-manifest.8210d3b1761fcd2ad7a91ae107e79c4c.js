bee7c700f45fc8ed6a1b62c32e570686ca0e4d30
fabipy <f.lang.1993@web.de>
2020-04-05 15:37:24 +0200

    Updates

---
+++ precache-manifest.8210d3b1761fcd2ad7a91ae107e79c4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e27370751fc8bc376e3481fe9c470d",
+    "url": "/rezepte/index.html"
+  },
+  {
+    "revision": "7a5aa71e09d33420eb0f",
+    "url": "/rezepte/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52b511364ffee6271cab",
+    "url": "/rezepte/static/js/2.d26762d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rezepte/static/js/2.d26762d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a5aa71e09d33420eb0f",
+    "url": "/rezepte/static/js/main.2e1a8ade.chunk.js"
+  },
+  {
+    "revision": "9b102e49bc007e68cec7",
+    "url": "/rezepte/static/js/runtime-main.95d842e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rezepte/static/media/logo.5d5d9eef.svg"
+  }
+]);
